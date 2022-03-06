a75e6b0a48d07d9cf596aa1ba1cc255556a670dd
Varis Rana <vrvarisrana123@gmail.com>
2022-03-06 06:24:01 +0530

    commit 06

--- Array/Array Easy/twoSum.java
+++ Array/Array Easy/twoSum.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class twoSum {
-      public int[] twoSum(int[] arr, int target) {
+      public static int[] twoSum(int[] arr, int target) {
             int[] ans = new int[2];
             for (int i = 0; i < arr.length; i++) {
                   for (int j = i + 1; j < arr.length; j++) {
