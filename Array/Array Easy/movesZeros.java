0ab205f7b595e717014294b4d33cccdac439ac6b
Varis Rana <vrvarisrana123@gmail.com>
2022-03-06 06:51:01 +0530

    commit 06

--- Array/Array Easy/movesZeros.java
+++ Array/Array Easy/movesZeros.java
@@ -1,4 +1,17 @@
 import java.util.*;
 public class movesZeros {
-      
+      public void Swap(int[] arr,int i,int j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+        public void moveZeroes(int[] nums) {
+         int n=nums.length;
+            int p=-1, itr=0;
+            while(itr<n){
+                if(nums[itr]>0)
+                    Swap(nums,++p,itr);
+                itr++;
+            }
+        }
 }
