e1b3c5a189e01f499981c8b11acc86245a20e5cc
Varis Rana <vrvarisrana123@gmail.com>
2022-03-06 08:04:07 +0530

    commit 06

--- Array/Array Easy/RunningSumof1dArray.java
+++ Array/Array Easy/RunningSumof1dArray.java
@@ -1,4 +1,12 @@
 import java.util.*;
 public class RunningSumof1dArray {
-      
+      public int[] runningSum(int[] nums) {
+            int ans=0;
+                 int[] temp=new int[nums.length];
+                 for(int i=0; i<nums.length; i++){
+                   ans=ans+nums[i];
+                     temp[i]=ans;
+                 }
+                 return temp;
+             }
 }
