9ef3fcc360f6a27de96d33358334e5dd8c4b50e0
Varis Rana <vrvarisrana123@gmail.com>
2022-03-20 12:13:56 +0530

    commit 20

--- TREE/Binary Tree/diameterBT.java
+++ TREE/Binary Tree/diameterBT.java
@@ -1,32 +1,41 @@
 import java.util.*;
+
 public class TreeNode {
-           int val;
-           TreeNode left;
-           TreeNode right;
-           TreeNode() {}
-           TreeNode(int val) { this.val = val; }
-           TreeNode(int val, TreeNode left, TreeNode right) {
-               this.val = val;
-               this.left = left;
-               this.right = right;
-           }
-       }
+      int val;
+      TreeNode left;
+      TreeNode right;
+
+      TreeNode() {
+      }
+
+      TreeNode(int val) {
+            this.val = val;
+      }
+
+      TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+      }
+}
+
 public class diameterBT {
       int max = 0;
-    
+
       public int diameterOfBinaryTree(TreeNode root) {
-          maxDepth(root);
-          return max;
+            maxDepth(root);
+            return max;
       }
-      
+
       private int maxDepth(TreeNode root) {
-          if (root == null) return 0;
-          
-          int left = maxDepth(root.left);
-          int right = maxDepth(root.right);
-          
-          max = Math.max(max, left + right);
-          
-          return Math.max(left, right) + 1;
+            if (root == null)
+                  return 0;
+
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+
+            max = Math.max(max, left + right);
+
+            return Math.max(left, right) + 1;
       }
 }
