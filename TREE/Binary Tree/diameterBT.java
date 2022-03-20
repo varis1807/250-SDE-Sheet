aff054098bd392d669d8c90333b4667a761138c8
Varis Rana <vrvarisrana123@gmail.com>
2022-03-20 12:22:45 +0530

    commit 20

--- TREE/Binary Tree/diameterBT.java
+++ TREE/Binary Tree/diameterBT.java
@@ -38,4 +38,23 @@ public class diameterBT {
 
             return Math.max(left, right) + 1;
       }
+
+      public int height(TreeNode node) {
+            return node == null ? -1 : Math.max(height(node.left), height(node.right)) + 1;
+      }
+
+      int diameter = 0;
+
+      public int diameterOfBinaryTree01(TreeNode root) {
+            if (root == null)
+                  return 0;
+
+            int ld = diameterOfBinaryTree(root.left);
+            int rd = diameterOfBinaryTree(root.right);
+
+            int lh = height(root.left);
+            int rh = height(root.right);
+
+            return Math.max(Math.max(ld, rd), lh + rh + 2);
+      }
 }
